dc64effb83dd5f177f8b40977dde838a42939f86
Tim Stullich <timstullich@gmail.com>
2015-07-08 13:44:44 -0700

    Changed node pointer to be unique

--- btree/Node.hh
+++ btree/Node.hh
@@ -1,25 +1,29 @@
 #ifndef __NODE_H_INCLUDED__
 #define __NODE_H_INCLUDED__
 
+#include <memory>
+
 template <class T>
 class Node
 {
     public:
         Node(T value);
-        std::shared_ptr<Node<T>> next;
+        std::unique_ptr<Node<T>> left;
+        std::unique_ptr<Node<T>> right;
         T value() const;
     private:
         T val;
 };
 
 template <typename T>
-using NodePtr = std::shared_ptr<Node<T>>;
+using NodePtr = std::unique_ptr<Node<T>>;
 
 template <class T>
 Node<T>::Node(T value)
 {
     val = value;
-    next = nullptr;
+    left = nullptr;
+    right = nullptr;
 }
 
 template <class T>
