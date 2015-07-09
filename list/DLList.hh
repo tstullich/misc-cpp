7ecd64f218e431755f0c428c82a8da56d7934a21
Tim Stullich <timstullich@gmail.com>
2015-07-08 18:09:07 -0700

    Added beginnings of doubly linked list class

---
+++ list/DLList.hh
@@ -0,0 +1,11 @@
+#ifndef __DLLLIST_H_INCLUDED__
+#define __DLLLIST_H_INCLUDED__
+
+#include "DLNode.hh"
+
+template <class T>
+class DLList
+{
+};
+
+#endif // __DLLLIST_H_INCLUDED__
---
+++ list/DLNode.hh
@@ -0,0 +1,42 @@
+#ifndef __DLNODE_H_INCLUDED__
+#define __DLNODE_H_INCLUDED__
+
+#include <memory>
+
+template <class T>
+class DLNode
+{
+    public:
+        DLNode(T value);
+        T value() const;
+        void set_next(std::unique_ptr<DLNode<T>> &value);
+        void set_prev(std::unique_ptr<DLNode<T>> &value);
+    private:
+        T val;
+        std::unique_ptr<DLNode<T>> next;
+        std::unique_ptr<DLNode<T>> previous;
+};
+
+template <typename T>
+using DLNodePtr = std::unique_ptr<DLNode<T>>;
+
+template <class T>
+DLNode<T>::DLNode(T value)
+{
+    val = value;
+    next = nullptr;
+    previous = nullptr;
+}
+
+template <class T>
+T DLNode<T>::value() const
+{
+    return val;
+}
+
+template <class T>
+void DLNode<T>::set_next(std::unique_ptr<DLNode<T>> &value)
+{
+    next = value;
+}
+#endif // __DLNODE_H_INCLUDED__
